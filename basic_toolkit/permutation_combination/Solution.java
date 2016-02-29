8dafbba2492ae69b2d3bb8439368524d238af203
texasred <texasred2013@hotmail.com>
2016-02-29 15:23:04 -0600

    kaihuang: I add 3 common implementations for the permutation template.

--- basic_toolkit/permutation_combination/Solution.java
+++ basic_toolkit/permutation_combination/Solution.java
@@ -36,13 +36,14 @@ public class Solution {
 		}
 		for (int i = start; i < n; i++) {
 			// 当前第一个元素 加入 i == start
-					// if (i > start && A[i] == A[i - 1]) continue;
+			// if (i > start && A[i] == A[i - 1]) continue;
 			row.add(A[i]);
 			combinationHelper(A, i + 1, result, row, k);
 			row.remove(row.size() - 1);
 		}
 	}
-
+	
+	// P(n, k)
 	public List<List<Integer>> permuteUnique(int[] nums, int k) {
 		List<List<Integer>> result = new ArrayList<>();
 		List<Integer> row = new ArrayList<>();
@@ -62,13 +63,81 @@ public class Solution {
 		}
 		for (int i = 0; i < n; i++) {
 			if (!visited[i]) {
-				//if (i != 0 && A[i] == A[i - 1] && !visited[i - 1]) continue;
+				if (i != 0 && A[i] == A[i - 1] && !visited[i - 1]) continue;
 				visited[i] = true;
 				row.add(A[i]);
 				permutationHelper(A, result, row, visited, k);
 				row.remove(row.size() - 1);
 				visited[i] = false;
 			}
+		}
+	}
+
+	// Assuming the elements are unique
+	public List<List<Integer>> permutation(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> row = new ArrayList<>();
+		if (nums == null || nums.length == 0) return result;
+		int n = nums.length;
+		
+		// Solution 1 
+		// helper1(nums, result, row);
+		
+		// Solution 2
+		// boolean[] visited = new boolean[n];
+		// helper2(nums, visited, result, row);
+		
+		// Solution 3 
+		List<Integer> list = new ArrayList<>();
+		for (int elem : nums) list.add(elem);
+		helper3(list, 0, result);
+		
+		return result;
+	}
+
+	// List contains
+	public void helper1(int[] A, List<List<Integer>> result, List<Integer> row) {
+		int n = A.length;
+		if (row.size() == n) {
+			result.add(new ArrayList<>(row));
+			return;
+		}    
+		for (int i = 0; i < n; i++) {
+			if (!row.contains(A[i])) {
+				row.add(A[i]);
+				helper1(A, result, row);
+				row.remove(row.size() - 1);          
+			}        
+		}
+	}
+	// Visited array
+	public void helper2(int[] A, boolean[] visited, List<List<Integer>> result, List<Integer> row) {
+		int n = A.length;
+		if (row.size() == n) {
+			result.add(new ArrayList<>(row));
+			return;
+		}    
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				row.add(A[i]);
+				visited[i] = true;
+				helper2(A, visited, result, row);
+				visited[i] = false;
+				row.remove(row.size() - 1);          
+			}        
+		}
+	}
+	// Swap based permutation
+	public void helper3(List<Integer> list, int start, List<List<Integer>> result) {
+		int n = list.size();
+		if (start == n) {
+			result.add(new ArrayList<>(list));
+		} 
+		for (int i = start; i < n; i++) {
+			// if (i > start && list.get(start) == list.get(i)) continue;
+			Collections.swap(list, start, i);
+			helper3(list, start + 1, result);
+			Collections.swap(list, start, i);
 		}
 	}
 }
